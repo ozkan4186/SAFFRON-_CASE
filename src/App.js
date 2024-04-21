e17d85f846da06c0deb7ff6e3d3d70018431aa81
Özkan Demir <ozkanndemir2009@gmail.com>
2024-04-21 16:06:29 +0300

    assets and css added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import gptLogo from "./assets/chatgpt.svg";
 
 function App() {
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="" className="logo" />
+            <span className="brand">ChatGpt</span>
+          </div>
+          <button className="midBtn">
+            <img src="" alt="" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src="" alt="" />
+              What is Programing ?
+            </button>
+            <button className="query">
+              <img src="" alt="" />
+              What is Programing ?
+            </button>
+          </div>
+        </div>
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main"></div>
+    </div>
+  );
 }
 
 export default App;

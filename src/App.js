0c538ead959c9e5207432966ac513d8bddc95944
Özkan Demir <ozkanndemir2009@gmail.com>
2024-04-22 01:16:35 +0300

    Function added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
 import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
+import sendBtn from "./assets/send.svg";
+import userIcon from "./assets/user-icon.png";
+import gptImgLogo from "./assets/chatgptLogo.svg";
+import { useState } from "react";
 
 function App() {
+  const [messages, setMessages] = useState([
+    { text: "Merhaba!", sender: "bot" },
+    { text: "Nasıl yardımcı olabilirim?", sender: "bot" },
+  ]);
+  const [message, setMessage] = useState("");
+
+  const handleMessageSubmit = () => {
+    const newMessage = { text: message, sender: "user" };
+    const botResponse = {
+      text: "Merhaba, nasıl yardımcı olabilirim?",
+      sender: "bot",
+    };
+    setMessages([...messages, newMessage, botResponse]);
+    setMessage("");
+  };
+
   return (
     <div className="App">
       <div className="sideBar">
@@ -11,23 +36,66 @@ function App() {
             <span className="brand">ChatGpt</span>
           </div>
           <button className="midBtn">
-            <img src="" alt="" className="addBtn" />
+            <img src={addBtn} alt="" className="addBtn" />
             New Chat
           </button>
           <div className="upperSideBottom">
             <button className="query">
-              <img src="" alt="" />
+              <img src={msgIcon} alt="" />
               What is Programing ?
             </button>
+            <br />
             <button className="query">
-              <img src="" alt="" />
-              What is Programing ?
+              <img src={msgIcon} alt="" />
+              How to Use an Apı?
             </button>
           </div>
         </div>
-        <div className="lowerSide"></div>
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src={home} alt="" className="listItemsImg" />
+            Home
+          </div>
+          <div className="listItems">
+            <img src={saved} alt="" className="listItemsImg" />
+            Saved
+          </div>
+          <div className="listItems">
+            <img src={rocket} alt="" className="listItemsImg" />
+            Upgrade to Pro
+          </div>
+        </div>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <div className="chats">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`chat ${msg.sender === "user" ? "right" : "left"}`}
+            >
+              <img
+                src={msg.sender === "user" ? userIcon : gptImgLogo}
+                className="chatImg"
+                alt=""
+              />
+              <p className="text">{msg.text}</p>
+            </div>
+          ))}
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input
+              type="text"
+              placeholder="Send a message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button className="send" onClick={handleMessageSubmit}>
+              <img src={sendBtn} alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
